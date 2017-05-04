7a846d454b6228e8c81cbf9a4cf2fc2ae8fa883f
Sidney Reis <sidney.reis@acad.pucrs.br>
2017-05-04 00:50:29 -0300

    Implementacao do objeto 'pronta'

--- downunderRMI/src/DownUnder.java
+++ downunderRMI/src/DownUnder.java
@@ -27,9 +27,7 @@ class Partida {
     public int[] ultimasJogadas; // [ Posição da última jogada | Posição da penúltima jogada ]
     public int estado;
 
-    public Partida(Jogador jogador1, Jogador jogador2) {
-        this.jogador1 = jogador1;
-        this.jogador2 = jogador2;
+    public Partida() {
         for(int i = 0; i < 5; i++) {
             for(int j = 0; j < 8; j++) {
                 this.tabuleiro[i][j] = '-';
@@ -50,8 +48,10 @@ public class DownUnder extends UnicastRemoteObject implements DownUnderInterface
 
     private static AtomicInteger idCounter = new AtomicInteger();
 
-    protected DownUnder() throws RemoteException {
+    public DownUnder() throws RemoteException {
     }
+
+    //TODO: rever métodos para incluir timeout e synchronized
 
     public static int novoIdJogador() {
         return idCounter.getAndIncrement();
@@ -106,7 +106,7 @@ public class DownUnder extends UnicastRemoteObject implements DownUnderInterface
                 return 1; // Partida existe e jogador é o 1 (inicia e usa as esferas "C")
             }
             else if(partida.jogador2.id == jogador.id) {
-                return 2; // Partida existe e jogador é o 1 (segundo a jogar e usa as esferas "E")
+                return 2; // Partida existe e jogador é o 2 (segundo a jogar e usa as esferas "E")
             }
 
             return -1;
@@ -130,7 +130,7 @@ public class DownUnder extends UnicastRemoteObject implements DownUnderInterface
                 return 0; // Não é a vez do jogador
             }
             else if(((partida.jogador1.id == jogador.id) && (partida.estado == 1)) ||
-            ((partida.jogador2.id == jogador.id) && (partida.estado == 2))) {
+                    ((partida.jogador2.id == jogador.id) && (partida.estado == 2))) {
                 return 1; // Vez do jogador
             }
 
@@ -146,8 +146,27 @@ public class DownUnder extends UnicastRemoteObject implements DownUnderInterface
         Partida partida = jogadores.get(idJogador).partidaAtual;
         StringBuilder topoTabuleiro = new StringBuilder();
 
+        if((partida.quantidadeOrificio[0] == 8) &&
+                (partida.quantidadeOrificio[1] == 8) &&
+                (partida.quantidadeOrificio[2] == 8) &&
+                (partida.quantidadeOrificio[3] == 8) &&
+                (partida.quantidadeOrificio[4] == 8)) {
+            for(int i = 0; i < 5; i++) {
+                for(int j = 0; j < 8; j++) {
+                    topoTabuleiro.append(partida.tabuleiro[i][j]);
+                }
+                topoTabuleiro.append("\n");
+            }
+            return String.valueOf(topoTabuleiro);
+        }
+
         for(int i = 0; i < 5; i++) {
-            //TODO: adicionar topo de orifícios na string topoTabuleiro
+            if(partida.quantidadeOrificio[i] == 8) {
+                topoTabuleiro.append(partida.tabuleiro[i][0]);
+            }
+            else {
+                topoTabuleiro.append("-");
+            }
         }
         for(int i = 0; i < 5; i++) {
             if(i == partida.ultimasJogadas[0] || i == partida.ultimasJogadas[1])
@@ -164,13 +183,54 @@ public class DownUnder extends UnicastRemoteObject implements DownUnderInterface
 
     @Override
     public int soltaEsfera(int idJogador, int orificioTorre) throws RemoteException {
-        return 0;
+        //TODO: TIMEOUT: retorna 2 (partida encerrada, o que ocorrerá caso o jogador demore muito para enviar a sua jogada e ocorra o time­out de 60 segundos para envio de jogadas)
+        Jogador jogador = jogadores.get(idJogador);
+        Partida partida = jogador.partidaAtual;
+
+        if(((partida.jogador1.id == jogador.id) && (partida.estado == 2)) ||
+                ((partida.jogador2.id == jogador.id) && (partida.estado == 1))) {
+            return -3; // Não é a vez do jogador
+        }
+        else if(partida.estado == 0) {
+            return -2; // Não há dois jogadores na partida ainda
+        }
+        else if((orificioTorre != 0) &&
+                (orificioTorre != 1) &&
+                (orificioTorre != 2) &&
+                (orificioTorre != 3) &&
+                (orificioTorre != 4)) {
+            return -1; // Número inválido de orifício
+        }
+        else if(partida.quantidadeOrificio[orificioTorre] == 8) {
+            return 0; // Movimento inválido: orifício já foi preenchido
+        }
+
+        char peca = 'C';
+        if(partida.jogador2.id == jogador.id) {
+            peca = 'E';
+        }
+
+        partida.tabuleiro[orificioTorre][7 - partida.quantidadeOrificio[orificioTorre]] = peca;
+        return 1; // Movimento realizado
     }
 
     @Override
     public String obtemOponente(int idJogador) throws RemoteException {
-        return null;
+        try {
+            Jogador jogador = jogadores.get(idJogador);
+            Partida partida = jogador.partidaAtual;
+
+            if (partida.estado == 0) {
+                return ""; // Erro: partida não possuí 2 jogadores ainda
+            }
+
+            if (partida.jogador1.id == jogador.id) {
+                return partida.jogador2.nome;
+            }
+            return partida.jogador1.nome;
+        }
+        catch (Exception e) {
+            return "";
+        }
     }
-
-
 }
